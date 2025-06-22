47ac1d2947eea24216c36e1a36a67ee3ba0c6088
Tu Nombre <jlarez.1645@unimar.edu.ve>
2025-06-21 20:09:54 -0400

    aa

--- index.php
+++ index.php
@@ -82,3 +82,152 @@
                         <p> Tortilla de espinacas y champiñones. </p>
                         <p> Sopa de verduras con pollo </p>
                         <p> Hamburguesa de pavo con lechuga y tomate. </p>
+  
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <section id="Resultados" class="results">
+            <h2 class="section-title"> Resultados que motivan </h2>
+            <div class="results-container">
+                <div class="results-card">
+                    <div class="results-img">
+                        <img src="imgs/img_resultado1.jpg" alt="resultado1">
+                    </div>
+                    <div class="results-info">
+                        <h1> Feliz y sana</h1>
+                        <p> Cada vez me siento mejor con mi cuerpo gracias a Vida Sana. Te dejo un link de un video que te puede motivar en este proceso</p>
+                        <p> - Ana Pérez </p>
+                        <div class="results.links">
+                            <a href="https://www.youtube.com/watch?v=AhI9uEgWmzM" target="_blank"> <i class="fas fa-link"> Video </i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="results-card">
+                    <div class="results-img">
+                        <img src="imgs/img_resultado2.jpg" alt="resultado2">
+                    </div>
+                    <div class="results-info">
+                        <h1>Mejorando cada dia</h1>
+                        <p> Sana y feliz, mejor imposible! Te dejaré un link de un video que puede ayudarte</p>
+                        <p> Camila Perez</p>
+                        <div class="results.links">
+                            <a href="https://www.youtube.com/watch?v=9VSO0vBk11E" target="_blank"> <i class="fas fa-link"> Video </i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="results-card">
+                    <div class="results-img">
+                        <img src="imgs/img_resultado3.jpg" alt="resultado3">
+                    </div>
+                    <div class="results-info">
+                        <h1> Un cambio total </h1>
+                        <p> Un cambio que no solo sube mi autoestima, sino que tembien me permite motivarte. Te dejo un link de un video que te puede animar a empezar ya!</p>
+                        <p> - Lucia Gonzalez </p>
+                        <div class="results.links">
+                            <a href="https://www.youtube.com/watch?v=mpefmz4a9Mg" target="_blank"> <i class="fas fa-link"> Video </i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="Comentarios" class="comments">
+            <h2 class="section-title"> COMENTARIOS </h2>
+            <div class="row align-items-start">
+                <div class="container-fluid row"></div>
+                <form class="col-3 p-3" method="POST">
+
+                    <h4> Registrate y deja tu nota pública</h4>
+                    <p> Tu opinión es muy importante para nosotros.</p>
+                    <p> ¿Qué te ha parecido nuestro servicio?</p>
+                    <?php
+                    include "model/conn.php";
+                    include "controller/new_user.php";
+                    include "controller/delete_user.php";
+                    include "controller/edit_user.php";
+
+                    ?>
+                    <div class="mb-3">
+                        <label for="nombre_label" class="form_label"> Escribe tu nombre y Apellido</label>
+                        <input type="text" class="form-control" name="nombreyapellido" id="nombre_label" placeholder="Nombre y Apellido" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="usuario_label" class="form_label"> Escribe tu usuario</label>
+                        <input type="text" class="form-control" name="usuario" id="nombre_label" placeholder="Usuario" required>
+                    </div>
+                    <div class="mb-4">
+                        <label for="email_label" class="form_label"> Escribe tu correo electrónico</label>
+                        <input type="email" class="form-control" name="email" id="email_label" placeholder="Correo Electrónico" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nota_label" class="form-label">Escribe tu nota</label>
+                        <textarea name="nota" placeholder="Por favor, escribe tu mensaje aqui"></textarea>
+                    </div>
+                    <button type="submit" class="btn_enviar" name="btnenviar" value="ok">ENVIAR</button>
+
+                </form>
+
+
+                <div class="col-5 p-3">
+                    <table class="table">
+                        <div class=" table-container">
+
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombre</th>
+                                    <th>Usuario</th>
+                                    <th>Email</th>
+                                    <th>Nota</th>
+                                    <th>Fecha</th>
+
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                include "model/conn.php";
+                                $sql = $conn->query("select * from usuarios");
+                                while ($datos = $sql->fetch_object()) { ?>
+                                    <tr>
+                                        <th> <?= $datos->ID ?> </th>
+                                        <th> <?= $datos->nombreyapellido ?></th>
+                                        <td><?= $datos->usuario ?></td>
+                                        <td><?= $datos->email ?></td>
+                                        <td><?= $datos->nota ?></td>
+                                        <td><?= $datos->fechanota ?></td>
+
+                                        <td>
+                                            <a href="edit_index.php?id=<?= $datos->ID ?>" class="btn btn-sm btn-warning"> <i class="fa-solid fa-pencil"></i></a>
+                                        </td>
+
+                                        <td>
+                                            <a onclick="return confirm('Está seguro que desea borrar el usuario?')" href="index.php?id=<?= $datos->ID ?>" class="btn btn-sm btn-danger"> <i class="fa-solid fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                    </table>
+                </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="footer-content">
+            <p> &copy; 2025 Vidasana- Todos los derechos reservados</p>
+            <div class="social-links">
+                <a href="#" target="_blank"> <i class="fab fa-instagram"> </i></a>
+                <a href="#" target="_blank"> <i class="fab fa-facebook"> </i></a>
+                <a href="#" target="_blank"> <i class="fab fa-twitter"> </i></a>
+                <a href="#" target="_blank"> <i class="fab fa-telegram"> </i></a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>                      
